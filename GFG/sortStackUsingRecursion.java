ef4032312720cd1a5315acba871657da171a236a
Aakash <akashdas7781@gmail.com>
2025-02-25 19:13:02 +0530

    sortStackUsingRecursion modified

--- GFG/sortStackUsingRecursion.java
+++ GFG/sortStackUsingRecursion.java
@@ -2,25 +2,39 @@ package DSA.GFG;
 
 import java.util.Stack;
 
-public class sortStackUsingRecursion{
+public class sortStackUsingRecursion {
 
-    public static void insertSorted(Stack<Integer> stack,int element){
-        if(stack.isEmpty() || stack.peek()<=element){
+    public static void insertSorted(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || stack.peek() <= element) {
             stack.push(element);
+        } else {
+            int top = stack.pop();
+            insertSorted(stack, element);
+            stack.push(top);
         }
-        stack.pop();
-        insertSorted(stack,element);
-        stack.push(top);
     }
-    public static Stack<Integer> sort(Stack<Integer> stack){
-        if(stack.isEmpty()){
-            return s;
+
+    public static Stack<Integer> sort(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return stack;
         }
-
-        int top=stack.pop();
+        int top = stack.pop();
         sort(stack);
-
-        insertSorted(stack,top);
+        insertSorted(stack, top);
         return stack;
     }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(5);
+        st.push(21);
+        st.push(41);
+        st.push(4);
+        st.push(32);
+        st.push(13);
+        Stack<Integer> sortedStack = sort(st);
+        System.out.println(sortedStack);
+
+    }
+
 }
