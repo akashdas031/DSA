48e38f5a0dbdf8420a01d168896391206d71db57
Aakash <akashdas7781@gmail.com>
2025-03-15 17:59:03 +0530

    trap rain water solved using stack

--- GFG/trapRainWater.java
+++ GFG/trapRainWater.java
@@ -1,6 +1,7 @@
 package DSA.GFG;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class trapRainWater {
     public static int maxWater(int arr[]) {
@@ -50,8 +51,41 @@ public class trapRainWater {
         }
         return result;
     }
+    //using stack
+   public static int maxWaterUsingStack(int[] arr) {
+
+        Stack<Integer> st = new Stack<>();
+        int res = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+           
+            // Pop all items smaller than arr[i]
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {          
+                
+                int pop_height = arr[st.pop()];
+
+                if (st.isEmpty())
+                    break;
+
+                // arr[i] is the next greater for the removed item
+                // and new stack top is the previous greater 
+                int distance = i - st.peek() - 1;
+
+                // Take the minimum of two heights (next and prev greater)
+                int water = Math.min(arr[st.peek()], arr[i]);
+
+                // Find the amount of water
+                water -= pop_height;
+                
+                res += distance * water;
+            }
+            st.push(i);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr={3,0,1,0,4,0,2};
-        System.out.println(maxWaterOptimal(arr));
+        System.out.println(maxWaterUsingStack(arr));
     }
 }
